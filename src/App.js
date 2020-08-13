075c56cfa9182ed92a1a3e913f53429a3ab0ac56
tbuddhi <tharakabuddhi@gmail.com>
2020-08-14 01:34:04 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchMovies from "./searchMovies";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <h1 className="title">React Movie Search</h1>
       <SearchMovies/>
     </div>
